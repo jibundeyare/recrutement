f2150615370ed182a2e19606493f8098de1edf9d
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2019-05-22 12:22:46 +0200

    Change uploaded file names
    Uploaded files were renamed using the following format :
    
    	uniqueId_originalName.zip
    
    But it was complicated to match the uploaded file with the owner without
    searching the admin screen for the file name.
    
    Now the files are renamed using the following format :
    
    	firstname_lastname_uniqueId_originaleName.zip

---
+++ src/Service/ApplicationNamer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\PropertyMapping;
+use Vich\UploaderBundle\Naming\ConfigurableInterface;
+use Vich\UploaderBundle\Naming\NamerInterface;
+use Vich\UploaderBundle\Util\Transliterator;
+
+/**
+ * ApplicationNamer
+ */
+class ApplicationNamer implements NamerInterface, ConfigurableInterface
+{
+    /**
+     * @var bool
+     */
+    private $transliterate = false;
+    /**
+     * @param array $options Options for this namer. The following options are accepted:
+     *                       - transliterate: whether the filename should be transliterated or not
+     */
+    public function configure(array $options): void
+    {
+        $this->transliterate = isset($options['transliterate']) ? (bool) $options['transliterate'] : $this->transliterate;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function name($object, PropertyMapping $mapping): string
+    {
+        /* @var $file UploadedFile */
+        $file = $mapping->getFile($object);
+        $name = $file->getClientOriginalName();
+        $firstname = $object->getFirstname();
+        $lastname = $object->getLastname();
+        if ($this->transliterate) {
+            $name = Transliterator::transliterate($name);
+            $firstname = Transliterator::transliterate($firstname);
+            $lastname = Transliterator::transliterate($lastname);
+        }
+        return $firstname.'_'.$lastname.'_'.\uniqid().'_'.$name;
+    }
+}
+
